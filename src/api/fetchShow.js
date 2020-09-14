04ccb2bea7b2d7d5cdc495379bb1173b364e6547
unknown <aelise17264@gmail.com>
2020-09-14 14:09:22 -0600

    mvp met

---
+++ src/api/fetchShow.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+export const fetchShow = () =>{
+return axios.get('https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes')
+.then(res =>{
+     return res.data
+})
+.catch(error => {
+    console.log('error in fetch')
+    return error
+})
+
+}
